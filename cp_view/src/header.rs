8b72d73d5b0f26f01846c641710df58e7c6d3de1
echozyr2001 <echo.zyr.2001@gmail.com>
2023-02-28 17:05:10 +0800

    add link and list display

--- cp_view/src/header.rs
+++ cp_view/src/header.rs
@@ -1,17 +1,18 @@
+use druid::widget::{Button, Label};
 use druid::{
-    widget::{Button, Flex, Padding},
+    widget::{Flex, Padding},
     Widget,
 };
+use druid::{LocalizedString, WidgetExt};
+use opener::open_browser;
 
 use crate::AppState;
-
-// struct Header;
 
 pub fn build() -> impl Widget<AppState> {
     Padding::new(
         1.0,
         Flex::row()
-            .with_child(Button::new("button1").on_click(|_, _, _| {
+            .with_child(Button::new("button1").on_click(|_, _data, _| {
                 // let mut log_info = self::AppState::log_info;
                 let mut tmp = vec!["a".to_string()];
                 tmp.push("b".to_string());
@@ -20,6 +21,16 @@ pub fn build() -> impl Widget<AppState> {
                 }
                 // self::AppState::line_number
             }))
+            .with_child(
+                Label::new(LocalizedString::new("Click here to visit Example.com")).on_click(
+                    |_, _, _| {
+                        open_browser(
+                            "https://echo-zyr-2001s-organization.gitbook.io/rust_complier/",
+                        )
+                        .unwrap();
+                    },
+                ),
+            )
             .with_child(Button::new("button2"))
             .with_child(Button::new("button3"))
             .with_child(Button::new("button4"))
--- cp_view/src/info_panel.rs
+++ cp_view/src/info_panel.rs
@@ -1,6 +1,6 @@
 use druid::{
-    widget::{Label, Split},
-    Env, Widget,
+    widget::{Label, List, Split},
+    Env, Widget, WidgetExt,
 };
 
 use crate::{AppState, FONT_SIZE};
@@ -8,6 +8,11 @@ use crate::{AppState, FONT_SIZE};
 pub fn build() -> impl Widget<AppState> {
     let output = Label::new(|data: &AppState, _: &Env| format!("Line {}", data.line_number))
         .with_text_size(FONT_SIZE);
-    let log = Label::new(format!("this is log")).with_text_size(FONT_SIZE);
+
+    let log = List::new(make_list).lens(AppState::log_info);
     Split::rows(output, log)
+}
+
+fn make_list() -> impl Widget<String> {
+    Label::dynamic(|s: &String, _| s.to_string())
 }
